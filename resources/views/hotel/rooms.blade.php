6fbc03d6cd866a7329bfa7ed8e3f22a3babf8adb
Faiza-60 <111574858+Mirhagujjar@users.noreply.github.com>
2025-01-10 04:28:37 -0800

    3 commit

--- resources/views/hotel/rooms.blade.php
+++ resources/views/hotel/rooms.blade.php
@@ -9,12 +9,13 @@
             <div class="card shadow">
                 <div class="card-body">
                     <h5 class="card-title">{{ $room->name }}</h5>
-                    <p>Email: {{ $room->email }}</p>
-                    <p>Phone: {{ $room->phone }}</p>
-                    <p>Room Type: {{ $room->room_type }}</p>
-                    <p>Guests: {{ $room->guests }}</p>
-                    <p>Check-in: {{ $room->check_in }}</p>
-                    <p>Check-out: {{ $room->check_out }}</p>
+                    <p><h4>Email:</h4> {{ $room->email }}</p>
+                    <p><h4>Phone:</h4> {{ $room->phone }}</p>
+                    <p><h4>Check-in:</h4> {{ $room->check_in }}</p>
+                    <p><h4>Check-out:</h4> {{ $room->check_out }}</p>
+                    <p><h4>Room Type:</h4> {{ $room->room_type }}</p>
+                    <p><h4>Guests:</h4> {{ $room->guests }}</p>
+                    
 
                     <!-- Edit Button -->
                     <a href="{{ route('hotel.edit', $room->id) }}" class="btn btn-warning">Edit</a>
